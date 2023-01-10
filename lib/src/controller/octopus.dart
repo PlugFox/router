ee5b6ba486757c2180b705cf2c5b18afa9cb2e3d
Mikhail Matiunin <mikhail.matiunin@cityplumbing.co.uk>
2023-01-10 23:12:11 +0400

    Update Octopus

--- lib/src/controller/octopus.dart
+++ lib/src/controller/octopus.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/widgets.dart';
 import 'package:meta/meta.dart';
 
+import '../parser/information_parser.dart';
+import '../provider/information_provider.dart';
 import '../route/octopus_route.dart';
 import '../state/octopus_state.dart';
 import 'octopus_delegate.dart';
 
 /// {@template octopus}
-///
+/// The main class of the package.
 /// {@endtemplate}
 abstract class Octopus {
   factory Octopus({required List<OctopusRoute> routes}) = OctopusImpl;
@@ -18,15 +20,29 @@ abstract class Octopus {
 @internal
 class OctopusImpl implements Octopus {
   factory OctopusImpl({required List<OctopusRoute> routes}) {
-    final delegate = OctopusDelegate();
+    final routeInformationProvider = OctopusInformationProvider();
+    final routeInformationParser = OctopusInformationParser();
+    final routerDelegate = OctopusDelegate();
+    final backButtonDispatcher = RootBackButtonDispatcher();
     return OctopusImpl._(
-      delegate: delegate,
+      routeInformationProvider: routeInformationProvider,
+      routeInformationParser: routeInformationParser,
+      routerDelegate: routerDelegate,
+      backButtonDispatcher: backButtonDispatcher,
     );
   }
 
   OctopusImpl._({
-    required RouterDelegate<OctopusState> delegate,
-  }) : config = RouterConfig<OctopusState>(routerDelegate: delegate);
+    required RouteInformationProvider routeInformationProvider,
+    required RouteInformationParser<OctopusState> routeInformationParser,
+    required RouterDelegate<OctopusState> routerDelegate,
+    required BackButtonDispatcher backButtonDispatcher,
+  }) : config = RouterConfig<OctopusState>(
+          routeInformationProvider: routeInformationProvider,
+          routeInformationParser: routeInformationParser,
+          routerDelegate: routerDelegate,
+          backButtonDispatcher: backButtonDispatcher,
+        );
 
   @override
   final RouterConfig<OctopusState> config;
